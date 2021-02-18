dec395f8270a1cebcccaac05bcd75744594e3431
Even <765305261@qq.com>
2021-02-18 14:02:53 +0800

    添加项目文件。

---
+++ Track_LK/useLK.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+#include <chrono>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		cout << "usage: useLK path_to_dataset" << endl;
+		return 1;
+	}
+
+	string path_to_dataset = argv[1];
+	string associate_file = path_to_dataset + "/associate.txt";
+
+	ifstream fin(associate_file);
+	if (!fin)
+	{
+		cerr << "I cann't find associate.txt!" << endl;
+		return 1;
+	}
+
+	string rgb_file, depth_file, time_rgb, time_depth;
+
+	// 因为要删除跟踪失败的点，使用list: 频繁插入/删除操作
+	list<Point2f> keypoints;      
+	Mat color, depth, last_color;
+
+	for (int idx = 0; idx < 100; idx++)
+	{
+		fin >> time_rgb >> rgb_file >> time_depth >> depth_file;
+		cout << "time_rgb " << time_rgb << " ";
+		cout << "rgb_file: " << rgb_file << "\n";
+		cout << "time_depth: " << time_depth << " ";
+		cout << "depth file: " << depth_file << "\n";
+
+		color = cv::imread(path_to_dataset + "/" + rgb_file, cv::IMREAD_COLOR);
+		depth = cv::imread(path_to_dataset + "/" + depth_file, cv::IMREAD_UNCHANGED);
+		if (color.empty() || depth.empty())
+		{
+			printf("[Err]: empty image or depth.\n");
+			return -1;
+		}
+
+		if (idx == 0)
+		{
+			// 对第一帧提取FAST特征点
+			vector<KeyPoint> kpts;
+			Ptr<Feature2D> detector = cv::FastFeatureDetector::create();
+			detector->detect(color, kpts);
+			for (auto kp : kpts)
+			{
+				keypoints.push_back(kp.pt);
+			}
+
+			// 记录前一帧图像
+			last_color = color;
+
+			continue;
+		}
+		if (color.data == nullptr || depth.data == nullptr)
+		{
+			continue;
+		}
+
+		// 对其他帧(从第二帧开始)用LK跟踪特征点
+		vector<Point2f> next_kpts;
+		vector<Point2f> prev_kpts;
+
+		// 从第二帧开始记录前一帧的关键点
+		for (auto kp : keypoints) 
+		{
+			prev_kpts.push_back(kp);
+		}
+
+		vector<unsigned char> status;
+		vector<float> error;
+		chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+
+		// ---------- 计算前后两帧之间的LK光流
+		cv::calcOpticalFlowPyrLK(last_color, color, prev_kpts, next_kpts, status, error);
+		// ----------
+
+		chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+		chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+		cout << "LK Flow use time：" << time_used.count() << " seconds." << endl;
+
+		// 把跟丢的点删掉
+		int i = 0;
+		for (auto iter = keypoints.begin(); iter != keypoints.end(); ++i)
+		{
+			if (status[i] == 0)
+			{
+				iter = keypoints.erase(iter);
+				continue;
+			}
+
+			*iter = next_kpts[i];
+			iter++;
+		}
+		cout << "tracked keypoints: " << keypoints.size() << endl;
+
+		if (keypoints.size() == 0)
+		{
+			cout << "all keypoints are lost." << endl;
+			break;
+		}
+
+		// 画出 keypoints
+		cv::Mat img_show = color.clone();
+		for (auto kp : keypoints)
+		{
+			cv::circle(img_show, kp, 10, cv::Scalar(0, 240, 0), 1);
+		}
+
+		cv::imshow("corners", img_show);
+		cv::waitKey(0);
+
+		// 记录前一帧图像
+		last_color = color;
+	}
+
+	return 0;
+}
